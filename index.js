a828674e92250e109a8c1aad88b1fcf840e75869
Kesban <k.ahmedowa94@gmail.com>
2022-07-19 11:06:44 +0200

    fertig

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ document.querySelector("#stand").innerHTML = stand + "€";
 
 function run(){
     const nummer = Math.round(Math.random() * 5 + 1);
+    switch(nummer){
+        case 1: document.querySelector("#wurffel").classList = "fa-solid fa-dice-one"; break;
+        case 2: document.querySelector("#wurffel").classList = "fa-solid fa-dice-two"; break;
+        case 3: document.querySelector("#wurffel").classList = "fa-solid fa-dice-three"; break;
+        case 4: document.querySelector("#wurffel").classList = "fa-solid fa-dice-four"; break;
+        case 5: document.querySelector("#wurffel").classList = "fa-solid fa-dice-five"; break;
+        case 6: document.querySelector("#wurffel").classList = "fa-solid fa-dice-six"; break;
+    }
 
-
-    document.querySelector("#nummer").innerHTML = nummer;
 
     if (nummer == 6){
         stand += 5;
+        
+
     } else {
         stand -= 1;
     };
